659ef55dfe7c662160eb767372ad406465c32dbf
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-04-20 09:28:33 -0600

    creating ContacModel

---
+++ Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApIContacts.Models
+{
+	public class Contact
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string UserName { get; set; }
+		[Required]
+		public EmailAddressAttribute email { get; set; }
+		[Required]
+		public byte[] Picture { get; set; }
+    }
+}
